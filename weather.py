c1ece9f7f0e626b523a9b89b4997c9251073678e
kacper.sienkiewicz <kacper.sienkiewicz@codilime.com>
2022-05-03 21:40:58 +0200

    test

---
+++ weather.py
@@ -0,0 +1,18 @@
+import requests
+
+API_KEY = "5a12f688ca668ab2b8e7ef2333abadae"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+requests_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(requests_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data["weather"][0]["description"]
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature)
+else:
+    print("An error occured")
